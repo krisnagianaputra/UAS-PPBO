bdc0e443b5ee2a16f321d044313fa1d800630e4b
krisnagianaputra <90766563+krisnagianaputra@users.noreply.github.com>
2021-10-20 19:02:15 -0700

    Commit GUI

---
+++ src/UASPrakPBO.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UASPrakPBO {
+    private JRadioButton barang1RadioButton;
+    private JRadioButton barang3RadioButton;
+    private JRadioButton barang2RadioButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JButton totalButton;
+    private JTextField textField16;
+    private JTextField textField17;
+    private JButton uangAkhirButton;
+    private JButton hapusSemuaButton;
+    private JButton keluarButton;
+
+    public UASPrakPBO() {
+        totalButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        uangAkhirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        hapusSemuaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        keluarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+    }
+}
